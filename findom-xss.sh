1046ea9b5dc6c63e63e10ef76121f9362bc4f69a
dw1 <dwi.siswanto98@gmail.com>
2020-05-09 20:19:49 +0700

    :zap: Move to main func & Add pipe read opt

--- findom-xss.sh
+++ findom-xss.sh
@@ -9,7 +9,7 @@ echo " | _|| | ' \| |) | (_) | |\/| |___>  <\__ \__ \\";
 echo " |_| |_|_||_|___/ \___/|_|  |_|  /_/\_\___/___/";
 echo -en "\n dw1@noobsec.org\n --"
 echo -en "\n Find for Possible DOM Based XSS Vulnerability"
-echo -e "\033[0m\n"
+echo -e "\033[0m"
 
 _findomXSS() {
 	PATTERN="(document|location|window)\.(URL|documentURI|href|search|hash|referrer|location\.href|name)"
@@ -33,23 +33,47 @@ _extractDomain() {
 	echo ${1} | cut -d'/' -f3 | cut -d':' -f1
 }
 
-MAIN="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
-RESULT="${MAIN}/results"
-[[ ! -d "${RESULT}" ]] && mkdir -p ${RESULT}
-_validateTarget ${1}
-DOMAIN=$(_extractDomain ${1})
-[[ -z "${2}" ]] && OUTPUT="${RESULT}/${DOMAIN}.txt" || OUTPUT="${2}"
-_healthCheck ${1} ${DOMAIN}
-echo "[$(date +'%m/%d/%Y %R')] Scanning against ${1}"
-_findomXSS ${1} ${OUTPUT} &
-python3 ${LINKFINDER} -d -i ${1} -o cli | grep -Eoi "https?://[^\"\\'> ]+" | grep "${DOMAIN}" | grep "\.js" | uniq |
-{
-	while IFS="" read -r JS
-	do
-		_findomXSS ${JS} ${OUTPUT} &
-	done
-
-	wait
-
-	[[ -f "${OUTPUT}" ]] && echo -e "\n\033[1mOutput saved into ${OUTPUT}\033[0m" || echo -e "\n\033[0;33mFound nothing. ¯\_(ツ)_/¯\033[0m"
+_help() {
+	echo -e "\n\033[0;31mPlease specify a target.\n\033[0m"
+	echo -e "\033[0;33mUsage:\n  ${0} http://target/\n  cat target.txt | ${0}\033[0m"
+	exit
 }
+
+_main() {
+	MAIN="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
+	RESULT="${MAIN}/results"
+	DOMAIN=$(_extractDomain ${1})
+
+	[[ ! -d "${RESULT}" ]] && mkdir -p ${RESULT}
+	[[ -z "${2}" ]] && OUTPUT="${RESULT}/${DOMAIN}.txt" || OUTPUT="${2}"
+	
+	_validateTarget ${1}
+	_healthCheck ${1} ${DOMAIN}
+	
+	echo -e "\n[$(date +'%m/%d/%Y %R')] Scanning against ${1}"
+	_findomXSS ${1} ${OUTPUT} &
+
+	python3 ${LINKFINDER} -d -i ${1} -o cli | grep -Eoi "https?://[^\"\\'> ]+" | grep "${DOMAIN}" | grep "\.js" | uniq |
+	{
+		while IFS="" read -r JS
+		do
+			_findomXSS ${JS} ${OUTPUT} &
+		done
+
+		wait
+
+		[[ -f "${OUTPUT}" ]] && echo -e "\n\033[1mOutput saved into ${OUTPUT}\033[0m\n-----" || echo -e "\n\033[0;33mFound nothing. ¯\_(ツ)_/¯\033[0m\n-----"
+	}
+}
+
+if [[ $# == 0 ]]; then
+	if [[ -p /dev/stdin ]]; then
+		while IFS="" read line; do
+			_main ${line}
+		done
+	else
+		_help
+	fi
+else
+	_main ${1} ${2}
+fi
